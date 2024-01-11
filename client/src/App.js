5fcd27c7469bf372470a0d05ed69c6e989372289
Freddy Perez <fred.perez15@gmail.com>
2024-01-10 21:21:15 -0800

    tailwind implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './Components/Home';
+import NavBar from './Components/NavBar';
 
 export default function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Home/>
     </div>
   );
---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,13 @@
+export default function NavBar() {
+    return(
+        <div>
+            <h1 
+                className="text-3xl font-bold underline 
+                border border-blue-400 
+                bg-red-400"
+                >
+                SMD
+            </h1>
+        </div>
+    )
+}
